c4cd8c759bac7d323f7bf9c5b9197a992b569736
Yusup <yusuprd@icloud.com>
2026-02-18 21:47:31 +0800

    Update homepage model and template

--- home/models.py
+++ home/models.py
@@ -1,7 +1,16 @@
 from django.db import models
-
 from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    template = "home/home_page.html"
+
+    subtitle = models.CharField(max_length=100, blank=True, null=True)
+    content = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('subtitle'),
+        FieldPanel('content'),
+    ]
