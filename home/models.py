eaa98a34fcabdfd432abe8815b3ff4177ad62878
Yusup <yusuprd@icloud.com>
2026-02-18 22:50:02 +0800

    Used wagtail images in template

--- home/models.py
+++ home/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
+from wagtail.images import get_image_model_string
 
 
 class HomePage(Page):
@@ -10,7 +11,16 @@ class HomePage(Page):
     subtitle = models.CharField(max_length=100, blank=True, null=True)
     content = RichTextField(blank=True)
 
+    image = models.ForeignKey(
+        get_image_model_string(),
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+
     content_panels = Page.content_panels + [
         FieldPanel('subtitle'),
         FieldPanel('content'),
+        FieldPanel('image'),
     ]
