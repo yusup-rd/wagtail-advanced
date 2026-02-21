6d6bdff1b73b79db9c8702921e82e12404c03038
Yusup <yusuprd@icloud.com>
2026-02-21 18:45:18 +0800

    Added custom pages

---
+++ blogpages/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BlogpagesConfig(AppConfig):
+    name = 'blogpages'
---
+++ blogpages/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+
+class BlogIndex(Page):
+    template = "blogpages/blog_index_page.html"
+
+    subtitle = models.CharField(max_length=255, blank=True)
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle"),
+        FieldPanel("body"),
+    ]
+
+
+class BlogDetail(Page):
+    subtitle = models.CharField(max_length=255, blank=True)
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle"),
+        FieldPanel("body"),
+    ]
