8f3f41033a970922eb687af7b269f20a732e906c
Yusup <yusuprd@icloud.com>
2026-02-28 20:47:12 +0800

    Added page context

--- blogpages/models.py
+++ blogpages/models.py
@@ -18,6 +18,12 @@ class BlogIndex(Page):
         FieldPanel("body"),
     ]
 
+    def get_context(self, request):
+        context = super().get_context(request)
+        context['blogpages'] = BlogDetail.objects.live(
+        ).public()
+        return context
+
 
 class BlogDetail(Page):
     subtitle = models.CharField(max_length=255, blank=True)
