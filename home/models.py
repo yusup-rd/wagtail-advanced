49b84bd59137e04e660861d0af7149b6b49ae715
Yusup <yusuprd@icloud.com>
2026-02-28 23:49:52 +0800

    Added cta linking with validation

--- home/models.py
+++ home/models.py
@@ -4,6 +4,7 @@ from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
 from wagtail.images import get_image_model_string
 from wagtail.documents import get_document_model_string
+from django.core.exceptions import ValidationError
 
 
 class HomePage(Page):
@@ -29,9 +30,38 @@ class HomePage(Page):
         related_name="+"
     )
 
+    cta_url = models.ForeignKey(
+        'wagtailcore.Page',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    cta_external_url = models.URLField(blank=True, null=True)
+
     content_panels = Page.content_panels + [
         FieldPanel('subtitle'),
+        FieldPanel('cta_url'),
+        FieldPanel('cta_external_url'),
         FieldPanel('content'),
         FieldPanel('image'),
         FieldPanel('custom_document'),
     ]
+
+    @property
+    def cta_urls(self):
+        if self.cta_url:
+            return self.cta_url.url
+        if self.cta_external_url:
+            return self.cta_external_url
+        else:
+            return None
+
+    def clean(self):
+        super().clean()
+
+        if self.cta_url and self.cta_external_url:
+            raise ValidationError(
+                "You cannot have both an internal and external CTA URL."
+            )
