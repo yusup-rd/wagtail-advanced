2405945b43382179d5c8f82bb4f95fbe352a5aa2
Yusup <yusuprd@icloud.com>
2026-03-01 23:50:58 +0800

    Added custom image formats for richtext

---
+++ images/image_formats.py
@@ -0,0 +1,4 @@
+from wagtail.images.formats import Format, register_image_format
+
+register_image_format(
+    Format('thumbnail', '150x150 thumbnail', 'richtext-image thumbnail-150', 'fill-150x150'),)
