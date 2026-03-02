fa37b0bda8f425b3f56b8255b050c174f179ab3f
Yusup <yusuprd@icloud.com>
2026-03-02 12:59:14 +0800

    Added custom context for streamblocks

--- blocks/blocks.py
+++ blocks/blocks.py
@@ -9,6 +9,12 @@ class TextBlock(blocks.TextBlock):
 
                          )
 
+    def get_context(self, value, parent_context=None):
+        context = super().get_context(value, parent_context=parent_context)
+        context["text"] = value
+        context["char_count"] = len(value or "")
+        return context
+
     class Meta:
         template = "blocks/text_block.html"
         icon = "doc-full"
@@ -21,11 +27,22 @@ class ImageBlock(ImageChooserBlock):
                          help_text="This is an image block. It can be used to display images to the reader."
                          )
 
+    def get_context(self, value, parent_context=None):
+        context = super().get_context(value, parent_context=parent_context)
+        context["image"] = value
+        context["has_image"] = value is not None
+        return context
+
     class Meta:
         template = "blocks/image_block.html"
 
 
 class InfoBlock(blocks.StaticBlock):
+    def get_context(self, value, parent_context=None):
+        context = super().get_context(value, parent_context=parent_context)
+        context["message"] = self.meta.admin_text
+        return context
+
     class Meta:
         # icon = "..."
         # template = "..."
@@ -37,10 +54,24 @@ class FAQBlock(blocks.StructBlock):
     question = blocks.CharBlock()
     answer = blocks.RichTextBlock(features=['bold', 'italic', 'link'])
 
+    def get_context(self, value, parent_context=None):
+        context = super().get_context(value, parent_context=parent_context)
+        question = value.get("question", "") if value else ""
+        context["question"] = question
+        context["answer"] = value.get("answer") if value else ""
+        context["question_anchor"] = question.strip().lower().replace(" ", "-")
+        return context
+
 
 class FAQListBlock(blocks.ListBlock):
     def __init__(self, **kwargs):
         super().__init__(FAQBlock(), **kwargs)
+
+    def get_context(self, value, parent_context=None):
+        context = super().get_context(value, parent_context=parent_context)
+        context["faqs"] = value
+        context["faq_count"] = len(value or [])
+        return context
 
     class Meta:
         # icon = "..."
@@ -56,6 +87,12 @@ class CarouselBlock(blocks.StreamBlock):
         ('text', blocks.TextBlock()),
         ('author', blocks.TextBlock()),
     ])
+
+    def get_context(self, value, parent_context=None):
+        context = super().get_context(value, parent_context=parent_context)
+        context["slides"] = value
+        context["slide_count"] = len(value or [])
+        return context
 
     class Meta:
         # icon = "..."
