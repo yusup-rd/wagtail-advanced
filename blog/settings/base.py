b8899300a6cd99b8ffc20500e7fc3fa29eb18f29
Yusup <yusuprd@icloud.com>
2026-03-17 14:37:37 +0800

    Modified search, added promoted search feature

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     "blocks",
     "settings",
 
+    "wagtail.contrib.search_promotions",
     "wagtail.contrib.forms",
     "wagtail.contrib.redirects",
     "wagtail.embeds",
