070f17e7581717aec7b528b3064709fd037795d0
Yusup <yusuprd@icloud.com>
2026-03-20 04:42:28 +0800

    Added custom admin panels and summary items

--- blogpages/wagtail_hooks.py
+++ blogpages/wagtail_hooks.py
@@ -1,4 +1,4 @@
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, mark_safe
 from wagtail.snippets.models import register_snippet
 from wagtail.snippets.views.snippets import SnippetViewSet
 from taggit.models import Tag
@@ -6,6 +6,8 @@ from django.core.cache import cache
 from django.contrib.auth.models import Permission
 from wagtail import hooks
 from blogpages.models import Author
+from wagtail.admin.ui.components import Component
+from wagtail.admin.site_summary import SummaryItem
 
 
 @register_snippet
@@ -46,3 +48,39 @@ def register_author_permissions():
         content_type__app_label='blogpages',
         codename='can_edit_author_name',
     )
+
+
+class WelcomePanel(Component):
+    order = 10
+    template_name = "panels/welcome_panel.html"
+
+    def get_context_data(self, parent_context):
+        context = super().get_context_data(parent_context)
+        context['request'] = parent_context['request']
+        context['username'] = parent_context['request'].user.username
+        return context
+
+    class Media:
+        css = {
+            'all': ['css/welcome_panel.css']
+        }
+
+
+@hooks.register('construct_homepage_panels')
+def add_welcome_panel(request, panels):
+    panels.append(WelcomePanel())
+
+
+class HomepageSummaryItem(SummaryItem):
+    order = 200
+    template_name = "panels/summary_item.html"
+
+    def get_context_data(self, parent_context):
+        context = super().get_context_data(parent_context)
+        context['purchases'] = 1000
+        return context
+
+
+@hooks.register('construct_homepage_summary_items')
+def add_homepage_summary_item(request, items):
+    items.append(HomepageSummaryItem(request))
